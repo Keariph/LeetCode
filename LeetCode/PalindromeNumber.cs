eb4202e2190c1c93dc5631b65548a8948eec5015
Ryhlinskaya <Ryhlinskaya@bk.ru>
2024-03-12 19:51:26 +0600

    Add two new solutions to easy tasks

---
+++ LeetCode/PalindromeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * [https://leetcode.com/problems/palindrome-number/description/]
+ */
+namespace LeetCode
+{
+    public class PalindromeNumber
+    {
+        public PalindromeNumber() { }
+
+        public bool Solution(int x)
+        {
+            if(x < 0) return false;
+            if (x >= 0 && x < 10) return true;
+
+            int number = x;
+            int polindrome = 0;
+
+            while (number > 0)
+            {
+                polindrome = (polindrome*10) + number%10;
+                number = number/10;
+            }
+
+            return polindrome == x;
+        }
+    }
+}
---
+++ LeetCode/RomanToInteger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * [https://leetcode.com/problems/roman-to-integer/description/]
+ */
+namespace LeetCode
+{
+    public class RomanToInteger
+    {
+        public RomanToInteger() { }
+
+        public int Solution(string s)
+        {
+            int result = 0;
+            Dictionary<char, int> romanToInteger = new Dictionary<char, int>();
+            romanToInteger['I'] = 1;
+            romanToInteger['V'] = 5;
+            romanToInteger['X'] = 10;
+            romanToInteger['L'] = 50;
+            romanToInteger['C'] = 100;
+            romanToInteger['D'] = 500;
+            romanToInteger['M'] = 1000;
+
+            int now = 0;
+            int next = 0;
+
+            for(int i = 0; i < s.Length - 1;  i++)
+            {
+                romanToInteger.TryGetValue(s[i], out now);
+                romanToInteger.TryGetValue(s[i +1 ], out next);
+
+                if (now < next)
+                {
+                    result -= now;
+                }
+                else
+                {
+                    result += now;
+                }
+            }
+
+            romanToInteger.TryGetValue(s[s.Length - 1], out now);
+            return result + now;
+        }
+    }
+}
